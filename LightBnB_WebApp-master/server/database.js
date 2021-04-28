4a04d2dcf7cacca55dea5899aac9d7ba616dab8d
Lily Habberfield <lilyhabbs@gmail.com>
2021-04-28 20:54:54 +0000

    Connect reservations function to database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -74,7 +74,16 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(`
+      SELECT *
+      FROM reservations
+      JOIN properties ON properties.id = reservations.property_id
+      WHERE guest_id = $1
+      LIMIT $2;
+    `, [guest_id, limit])
+    .then((res) => res.rows)
+    .catch((err) => console.log(err.message));
 }
 exports.getAllReservations = getAllReservations;
 
